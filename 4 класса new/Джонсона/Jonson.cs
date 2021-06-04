e7f191c340eb408bfbb4cbefa18674815641b645
Алексей Колесов <alladin.kolesov@mail.ru>
2021-06-04 20:04:01 +0300

    Для Лизы

--- 4 класса new/Джонсона/Jonson.cs
+++ 4 класса new/Джонсона/Jonson.cs
@@ -1,4 +1,4 @@
-﻿    using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -14,42 +14,46 @@ namespace _4_класса_new
         public void vvod()
         {
             int[] ms1 = { }, ms2 = { };
-            //Читаем
+            //Считываем с файла DjonsonVvod
             using (StreamReader sr = new StreamReader("DjonsonVvod.csv"))
             {
                 while (sr.EndOfStream != true)
                 {
-                    string str1 = sr.ReadLine();
-                    string str2 = sr.ReadLine();
-                    ms1 = Array.ConvertAll(str1.Split(';'), int.Parse);
-                    ms2 = Array.ConvertAll(str2.Split(';'), int.Parse);
+                    string str1 = sr.ReadLine(); // считываем в 2 строчки т.к. в условии всего 2 строчки и это самый оптимальный вариант
+                    string str2 = sr.ReadLine(); // считываем 2 строку
+                    ms1 = Array.ConvertAll(str1.Split(';'), int.Parse); // парсим в массив
+                    ms2 = Array.ConvertAll(str2.Split(';'), int.Parse); // парсим вторую строку во второй массив
                 }
             }
-            //Количество деталей
+            //Количество деталей (общее число столбцов по документу, необходим для дальнейшего проведение циклов, как ограничение)
             int detal = ms1.Length;
-            //Массив деталей
+            //Массив деталей (размерность 2 - количество станков по стандартным условиям, detail ограничение взято выше)
             int[,] mas = new int[2, detal];
-            //Из 1 в двумерный 
+            //Из одномерного в двумерный массив (те строки которые парсили в массив который создали на 31 строке)
             for (int i = 0; i < detal; i++)
             {
                 mas[0, i] = ms1[i];
                 mas[1, i] = ms2[i];
             }
-            this.mas = mas;
-            d = detal;
-            obrab();
+
+            this.mas = mas; // переносим текуще-сформированный массив в глобальную переменную
+            d = detal; // переносим количество деталей в глобальную переменную
+            obrab(); // запускаем метод обработки
         }
         public void obrab()
         {
-            //d- счетчик dd-тоже
+            //d - вытягиваем из глобальной переменной для проведение счетчиков, dd - такой же счетчик, в дальнейшем его будем просто убавлять программно не обнуляя
             int d = this.d, dd = this.d;
+            // формируем массив "деталей", такой же формировали выше
             int[,] mas = new int[2, d];
-            //счетчик и мин
+            //sch - счетчик (нужен чисто для красоты перебора шагов), min - необходим для проведения основного алгоритма программы
             int min, sch = 1;
-            int max = this.mas[0, 0];
-            //Счетчики для перестановки минимумов
+            int max = this.mas[0, 0]; // необходим для обнуления min эемента
+
+            //счетчики для перестановки минимумов
             int ii = 0;
             int jj = 0;
+
             //Поиск максимума
             for (int i = 0; i < 2; i++)
             {
@@ -61,7 +65,8 @@ namespace _4_класса_new
                     }
                 }
             }
-            //Поиск минимума на шагах
+
+            //Поиск минимума на иттерацияъ
             for (int g = 0; g < dd; g++)
             {
                 min = max;
@@ -72,26 +77,29 @@ namespace _4_класса_new
                         if (min > this.mas[i, j] && this.mas[i, j] != 0)
                         {
                             min = this.mas[i, j];
-                            ii = i;//Поиск номера станка
+                            ii = i;//Поиск номера станка (далее по нему производим перестановку)
                             jj = j;
                         }
                     }
                 }
+
                 //Перестановка
-                //Если станок первый, то 
+                //Если станок первый, то ставим деталь в начала очереди (массива)
                 if (ii == 0)
                 {
                     mas[0, g] = this.mas[0, jj];
                     mas[1, g] = this.mas[1, jj];
                 }
-                //Если второй то перестановка в конец
+                //Если второй то перестановка в конец очереди (массива)
                 else
                 {
                     mas[0, dd - 1] = this.mas[0, jj];
                     mas[1, dd - 1] = this.mas[1, jj];
-                    dd--;
-                    g--;
+                    dd--; //уменьшаем общий счетчик ограничения для коректной работы программы
+                    g--; // уменьшаем размер счетчика, т.к. элемент ставили в конец
                 }
+
+                // пошаговый вывод решения
                 Console.WriteLine("\n\n{0} шаг\n", sch);
                 for (int i = 0; i < 2; i++)
                 {
@@ -105,14 +113,15 @@ namespace _4_класса_new
                 this.mas[1, jj] = 0;
                 sch++;
             }
+
             //Временной массив для подсчета простоя 2 станка
             int[] vremia = new int[d];
-            int forVRE = 0;//Счетчик для времени 
-            vremia[forVRE] = mas[0, 0];//Первый простой(всегда)
+            int forVRE = 0;//Счетчик для времени
+            vremia[forVRE] = mas[0, 0];//Первый простой(всегда берется, и будет как минимум он, чисто по условию решения задачи)
             int sum = 0;
-            for (int o = 1; o < d; o++)//Первый просто учтен, поэтому 1.
+            for (int o = 1; o < d; o++)//Первый просто учтен, поэтому счетчик идет с 1.
             {
-                //Сумма первой строки
+                //Сумма первой строки (считается с учтением условий решения задач)
                 for (int j = 0; j < o; j++)
                 {
                     sum += mas[0, j];
@@ -126,7 +135,10 @@ namespace _4_класса_new
                 vremia[forVRE] = sum;
                 sum = 0;
             }
-            int maxProst = vremia.Max();//Максимальный простой
+
+            int maxProst = vremia.Max();//Максимальный простой из всех возможных простоев
+
+            //вывод на экран
             Console.WriteLine("\n\n\nОкончательное распределение: \n");
             for (int i = 0; i < 2; i++)
             {
@@ -137,7 +149,11 @@ namespace _4_класса_new
                 Console.WriteLine();
             }
             Console.WriteLine("Время простоя: " + maxProst);
-            using (StreamWriter sw = new StreamWriter("DjonsonOTV.csv", false, Encoding.Default, 10))
+
+
+            using (StreamWriter sw =
+
+            new StreamWriter("DjonsonOTV.csv", false, Encoding.Default, 10))
             {
                 for (int i = 0; i < 2; i++)
                 {
@@ -147,7 +163,7 @@ namespace _4_класса_new
                     }
                     sw.Write("\n");
                 }
-                sw.Write("Время; простоя:;" + maxProst);
+                sw.Write("Время простоя:" + maxProst);
             }
         }
     }
